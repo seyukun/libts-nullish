b2fbc7cdf6ff658b0de1f37e2b0e987eb0fb07d8
yush <yus-sato@kalyte.ro>
2025-11-23 17:57:09 +0000

    update: support boolean

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export const nullish = <T>(i: T | null | undefined): T | null => {
   if (typeof i === "undefined") return null;
   if (typeof i === "number" && Number.isNaN(i)) return null;
+  if (typeof i === "boolean" && i === false) return null;
   if (i === null) return null;
   return i;
 };
